2620b75d9784f0b033670eecda5864ffe155282f
tinchoz8426 <76183907+tinchoz8426@users.noreply.github.com>
2022-01-30 14:25:19 -0300

    contador

---
+++ js/contador.js
@@ -0,0 +1,28 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const fechaActual = new Date()
+console.log(fechaActual);
+
+const campusFecha = new Date(2022, 3, 11)
+console.log(campusFecha);
+function actualizar() {
+    const current = new Date()
+    const difference = campusFecha - current
+
+    const d = Math.floor(difference / 1000 / 60 / 60 / 24);
+    const h = Math.floor(difference / 1000 / 60 / 60) % 24;
+    const m = Math.floor(difference / 1000 / 60) % 60;
+    const s = Math.floor(difference / 1000) % 60;
+
+    days.innerHTML = d
+    hours.innerHTML = h
+    minutes.innerHTML = m
+    seconds.innerHTML = s
+}
+
+setInterval(() => {
+    actualizar()
+}, 1000);
